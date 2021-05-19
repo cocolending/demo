f031d74635c2833c2da7ef79c146ae508984ee24
coco <cocolending@protonmail.com>
2021-05-19 11:53:16 +0800

    ctoken class

--- src/compound/compound.js
+++ src/compound/compound.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import LPAddress from "./address.json";
 import IERC20 from "./abi/EIP20Interface.json";
-import IComptroller from "./abi/CErc20.json";
+import IComptroller from "./abi/ComptrollerG6.json";
 import CERC20 from "./abi/CErc20.json";
 import IOracle from "./abi/SimplePriceOracle.json";
 
@@ -16,31 +16,128 @@ const IERC20Template = new ethers.Contract('0x0000000000000000000000000000000000
 const IComptrollerTemplate = new ethers.Contract('0x0000000000000000000000000000000000000000', IComptroller);
 const IOracleTemplate = new ethers.Contract('0x0000000000000000000000000000000000000000', IOracle);
 
+export class CToken {
+    constructor(cToken, underlying, comptroller, oracle) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        this.cToken = cTokenTemplate.attach(cToken).connect(signer);
+        this.underlying = IERC20Template.attach(underlying).connect(signer);
+        this.comptroller = IComptrollerTemplate.attach(comptroller).connect(signer);
+        this.oracle = IOracleTemplate.attach(oracle).connect(signer);
+    }
+    async getAccount() {
+        return window.ethereum.enable().then(accs=>accs[0]);
+    }
 
-export const contracts = {
-    LP:[
+    // 存款
+    async mint(amount) {
+        const account = await this.getAccount();
+        const allowance = await this.underlying.allowance(account, this.cToken.address);
+        if (allowance.lt(amount))
+            await this.enableMarket();
+        await this.cToken.mint(amount)
+    }
+    // 取款
+    async redeem(amount, useCtoken = false) {
+        if (useCtoken)
+            await this.cToken.redeem(amount);
+        else
+            await this.cToken.redeemUnderlying(amount);
+    }
+
+    // 借款
+    async borrow(amount) {
+        await this.cToken.borrow(amount);
+    }
+    // 归还借款
+    async repayBorrow(amount) {
+        const account = await this.getAccount();
+        const allowance = await this.underlying.allowance(account, this.cToken.address);
+        if (allowance.lt(amount))
+            await this.enableMarket();
+        await this.cToken.repayBorrow(amount);
+    }
+
+    // 启用抵押
+    async enterMarket() {
+        await this.comptroller.enterMarkets([this.cToken.address])
+    }
+    // 获取抵押是否开启
+    async getMarketEntered() {
+        const account = await this.getAccount();
+        return await this.comptroller.checkMembership(account, this.cToken.address);
+    }
+
+    // 启用市场
+    async enableMarket() {
+        await this.underlying.approve(this.cToken.address, ethers.constants.MaxUint256);
+    }
+    // 市场是否启用
+    async getMarketEnabled() {
+        const account = await this.getAccount();
+        const threshold = 0;
+        const allowance = await this.underlying.allowance(account, this.cToken.address);
+        return allowance.gt(threshold);
+    }
+
+    // 借款上限
+    async getBorrowLimit() {
+        return 0;
+    }
+
+    // 当前市场总借款(未偿还+利息)
+    async getBorrowBalance() {
+        return await this.cToken.callStatic.totalBorrowsCurrent();
+    }
+    // 当前市场存款余额
+    async getSupplyBalance() {
+        return await this.cToken.getCash();
+    }
+    // 当前市场风险金
+    async getReserve() {
+        return await this.cToken.totalReserves();
+    }
+
+    // 查询账户存借状态
+    async getAccountSnapshot() {
+        const account = await this.getAccount();
+        const snap = await this.cToken.getAccountSnapshot(account);
+        if(Number(snap[0]) != 0) throw `getAccountSnapshot ${snap}`;
+        return snap.slice(1);
+    }
+    // 存款APY 年化
+    async getSupplyAPY() {
+        const perBlock = await this.cToken.supplyRatePerBlock();
+        const blockTime = 2;
+        return (Number(perBlock.mul(365 * 24 * 3600 / blockTime)) / 1e18 + 1) ** 365 - 1;
+    }
+    // 借款APY 年化
+    async getBorrowAPY() {
+        const perBlock = await this.cToken.borrowRatePerBlock()
+        const blockTime = 2;
+        return (Number(perBlock.mul(365 * 24 * 3600 / blockTime)) / 1e18 + 1) ** 365 - 1;
+    }
+
+}
+
+
+export const contracts = [
     {
         name: "BTC",
         decimals: 8,
-        underlying: IERC20Template.attach(LPAddress.BTC.underlying).connect(signer),
-        cToken: cTokenTemplate.attach(LPAddress.BTC.cToken).connect(signer),
+        lp: new CToken(LPAddress.BTC.cToken, LPAddress.BTC.underlying, LPAddress.comptroller, LPAddress.oracle)
     },
-     {
+    {
         name: "ETH",
         decimals: 18,
-        underlying: IERC20Template.attach(LPAddress.ETH.underlying).connect(signer),
-        cToken: cTokenTemplate.attach(LPAddress.ETH.cToken).connect(signer),
+        lp: new CToken(LPAddress.ETH.cToken, LPAddress.ETH.underlying, LPAddress.comptroller, LPAddress.oracle)
     },
     {
         name: "USDT",
         decimals: 18,
-        underlying: IERC20Template.attach(LPAddress.USDT.underlying).connect(signer),
-        cToken: cTokenTemplate.attach(LPAddress.USDT.cToken).connect(signer),
+        lp: new CToken(LPAddress.USDT.cToken, LPAddress.USDT.underlying, LPAddress.comptroller, LPAddress.oracle)
     }
-    ],
-    Comptroller: IComptrollerTemplate.attach(LPAddress.comptroller).connect(signer),
-    Oracle: IOracleTemplate.attach(LPAddress.oracle).connect(signer)
-}
+];
 window.contracts = contracts;
 
 
@@ -50,5 +147,7 @@ mint:
     ctoken.mint(amount);
 borrow:
     prepare: Comptroller.
+APY:
+    cToken.borrowRatePerBlock();
 
 */
